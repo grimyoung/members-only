c94d31f97ef73dadd651be4f9fedbf92eaed2386
grimyoung <samhyukim@gmail.com>
2019-04-20 16:56:14 -0700

    test for user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,22 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example User", email:"ex@example.com", 
+                    password: "foo", password_confirmation: "foo")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long " do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
