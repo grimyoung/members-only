d40b0eb304a1f560a3b73d0c0dd5b7ff800558ed
grimyoung <samhyukim@gmail.com>
2019-05-27 16:00:07 -0700

    post new action and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
+  before_action :logged_in? , only: [:new, :create]
+  
   def new
+    @post = Post.new
+  end
+
+  def create
+  
+  end
+
+  def index
+    @post = Post.all
   end
 end
